df19ec6a5fe2b1c035689d6bb64280c53277a2ef
moyenmutasim <moyenmutasim@users.noreply.github.com>
2020-10-17 16:42:30 +0800

    Update Checker.java
    Added bracketChecking and operatorChecking methods

--- Checker.java
+++ Checker.java
@@ -1,3 +1,40 @@
 public class Checker {
-    
+
+    public boolean check(String[] exps) {
+        for (String e : exps) {
+            char[] exp = e.toCharArray();
+            if (!checkBrackets(exp)) {
+                return false;
+            }
+            if(!checkOperators(exp)){
+
+            }
+        }
+    }
+
+    private boolean checkOperators(char[] exp) {
+        if(exp[0]=='+'||exp[0]=='-'||exp[0]=='*'||exp[0]=='/')
+        for(int i = 0 ; i<exp.length; i++){
+            if(exp[i]=='+'||exp[i]=='-'){
+                if(){
+
+                }
+            }
+        }
+    }
+
+    public boolean checkBrackets(char[] exp) {
+        int counter = 0;
+        for (char e : exp) {
+            if (e == '(') {
+                counter++;
+            } else if (e == ')') {
+                counter--;
+            }
+        }
+        if (counter != 0) {
+            return false;
+        }
+        return true;
+    }
 }
