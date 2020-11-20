aa4cc9d96adb175bd5e0d66cf4256c9103a334ca
waleedmq <60854828+waleedmq@users.noreply.github.com>
2020-11-20 15:43:37 +0800

    Create PostValidator.java

---
+++ input/PostValidator.java
@@ -0,0 +1,40 @@
+package input;
+public class PostValidator{
+
+    private static PostValidator instance = new PostValidator();
+
+	public static PostValidator getInstance() {
+		return instance;
+		
+    }
+
+    public int checkDegree(String eq){
+        char[] exp = eq.toCharArray();
+        int degree=0;
+        boolean lessthan2=true;
+        
+        for(int i=0;i<exp.length;i++){
+            if(exp[i]=='^'){
+                lessthan2=false;
+            }
+        }
+        
+        if(lessthan2) {
+        	for(int i=0;i<exp.length;i++) {
+        		if(Character.toString(exp[i]).matches("[A-Za-z]")){
+        			degree=1;
+        		}
+        	}
+        }
+        
+        if(!lessthan2) {
+        	for(int i=0;i<exp.length-1;i++) {
+        		if(exp[i]=='^') {
+        			degree=Math.max(degree,Character.getNumericValue(exp[i+1]));
+        		}
+        	}
+        }
+        
+        return degree;
+    }
+}
