cd97cb5507b50f8f08b7e0d8c20f61c3c43bdaec
moyenmutasim <moyenmutasim@users.noreply.github.com>
2020-11-21 16:55:00 +0800

    added add term to equation

--- variableTests/ConstructorVariableTerm.java
+++ variableTests/ConstructorVariableTerm.java
@@ -9,21 +9,18 @@ import factory.*;
 
 import equation.Term;
 import equation.VariableTerm;
-public class getTermsTest{
+public class ConstructorVariableTerm{
 	public String ListToString(ArrayList<Term> terms) {
 		return terms.get(0).toString();
 	}
 	
 	@Test
-	public void testToString() {
-		VariableTerm result = new VariableTerm("");
-		result.setCoefficient(2);
-		result.setVar('x');
-		result.setPower(1);
-		assertEquals("2.0x^1", result.toString());
+	public void testConstructor1() {
+		VariableTerm result = new VariableTerm("22x");
+		assertEquals("22.0x^1", result.toString());
 	}
 	@Test
-	public void testConstructor() {
+	public void testConstructor2() {
 		VariableTerm result = new VariableTerm("22x^2");
 		assertEquals("22.0x^2", result.toString());
 	}
