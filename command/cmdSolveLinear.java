bec475483a2520646b503f615251152ba80e48b3
moyenmutasim <moyenmutasim@users.noreply.github.com>
2020-11-26 22:45:36 +0800

    Update cmdSolveLinear.java

--- command/cmdSolveLinear.java
+++ command/cmdSolveLinear.java
@@ -8,6 +8,7 @@ import equation.QuadraticEquation;
 import equation.SimultaneousEquation;
 import factory.factoryCubic;
 import factory.factorySimultaneous;
+import input.InputProcessor;
 import input.InputValidator;
 import input.TextInput;
 
@@ -22,19 +23,22 @@ public class cmdSolveLinear implements Command {
 	@Override
 	public void execute() {
 		ArrayList<String> inEqs = new ArrayList<>();
-		System.out.println("Enter a list of equations with degree 1 (type \"done\" to stop):");
 		String temp;
 		Scanner in = TextInput.getInstance().getScanner();
 		boolean valid = true;
 		do {
 			temp = in.nextLine();
-			inEqs.add(temp);
-		} while (!temp.equalsIgnoreCase("done"));
+			inEqs.add(InputProcessor.getInstance().handleInput(temp));
+		} while (!temp.equals("done"));
 		inEqs.remove(inEqs.size());
 		for (String eq : inEqs) {
 			if (!InputValidator.getInstance().isValid(eq)) {
 				valid = false;
 			}
+		}
+		
+		for(int i = 0;i<inEqs.size();i++) {
+			inEqs.set(i, InputProcessor.getInstance().changeBrackets(inEqs.get(i)));
 		}
 
 		if (valid) {
