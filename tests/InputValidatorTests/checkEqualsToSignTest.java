7a0e1e7c6df4db8b5332872e682bb92d343cd606
waleedmq <60854828+waleedmq@users.noreply.github.com>
2020-11-29 17:34:47 +0800

    Create checkEqualsToSignTest.java

---
+++ tests/InputValidatorTests/checkEqualsToSignTest.java
@@ -0,0 +1,40 @@
+package testInputHandler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import input.*;
+
+public class checkEqualsToSignTest{
+
+	@Test
+	public void test1() {
+		InputValidator inputValidator = InputValidator.getInstance(); 
+		String[] input = {"=","+"};
+		boolean result=inputValidator.checkEqualsToSign(input);
+		assertEquals(false,result);
+	}
+	
+	@Test
+	public void test2() {
+		InputValidator inputValidator = InputValidator.getInstance(); 
+		String[] input = {"+","="};
+		boolean result=inputValidator.checkEqualsToSign(input);
+		assertEquals(false,result);
+	}
+	
+	@Test
+	public void test3() {
+		InputValidator inputValidator = InputValidator.getInstance(); 
+		String[] input = {"x","+","1","=","=","1"};
+		boolean result=inputValidator.checkEqualsToSign(input);
+		assertEquals(false,result);
+	}
+	
+	@Test
+	public void test4() {
+		InputValidator inputValidator = InputValidator.getInstance(); 
+		String[] input = {"x","+","1","=","0"};
+		boolean result=inputValidator.checkEqualsToSign(input);
+		assertEquals(true,result);
+	}
+}
